5eafd40b0bb2148e9788f61ca8861b50b9c001b8
blukaz <bare.luka.zagar@gmail.com>
2025-03-13 12:44:40 +0100

    fixed folder naming issue

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
+import pathlib
+
+# Read the README file with explicit UTF-8 encoding
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="ventral",  # Change this from "ventral-api-client" to "ventral"
-    version="0.1.2",  # Increment the version
+    version="0.1.3",  # Increment the version
     packages=find_packages(),
     install_requires=[],  # List dependencies here
     author="Bare Luka Zagar",
